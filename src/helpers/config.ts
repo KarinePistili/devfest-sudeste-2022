edf50e9adffc6355d3a7c8e0515d431f7ce73ed0
karine <karine.pistili@gmail.com>
2022-10-18 20:59:24 -0300

    updated and displayed register link

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,7 +1,7 @@
 const configValues = {
   name: 'Devfest Sudeste 2022',
   eventDate: '2022-12-16T20:00:00',
-  eventLinkRegistrationUrl: '',
+  eventLinkRegistrationUrl: 'https://www.sympla.com.br/devfest-sudeste-2022__1761249?share_id=7enma',
   place: 'Online',
   formattedDate: '16 e 17 de Dezembro 2022'
 }
